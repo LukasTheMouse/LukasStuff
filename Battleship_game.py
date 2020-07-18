7ed0561df391cd306ebc37af3d2b7e413533af9b
Lukas Muijs <lmuijs@trading.imc.intra>
2020-07-18 14:01:36 +0200

    Final commit

--- Battleship_game.py
+++ Battleship_game.py
@@ -1,42 +1,56 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[3]:
 
 
 import random as rand
+import click
 
 ships = {'S':1,'D':2,'C':3,'B':4,'A':5}
 
 names_ships = {'S':'Submarine','D':'Destroyer','C':'Cruiser','B':'Battleship','A':'Carrier'}
 
 
-# In[2]:
+# In[4]:
 
 
 def determine_size():
-    while True:
-        try:
-            value = int(input("""How big should I make the playboard? \n provide me with a number between 10-12 to define the witdh and length of the board.
-            \n\n\n Type 99 to exit the game \n\n"""))
-        except ValueError:
-            print("Sorry, I didn't understand that.")
-            continue
-
-        if value == 99:
-            print('You are a coward captain you are leaving our men behind')
-            break 
-        elif value < 9:
-            print("Sorry, your response must fall between 10 and 12")
-            continue
-        elif value > 12:
-            print("Sorry, your response must fall between 10 and 12")
-        else:
-            break
-    return value
+    value = click.prompt("""How big should I make the playboard? \n provide me with a number between 10-12 to define the witdh and length of the board.
+            \n\n\n Type 13 to exit the game \n\n""", type=click.IntRange(10, 13))
+    if value == 13:
+        print('See you later captain chickenshit :)')
+        exit()
+    else:
+        return value
 
 
-# In[3]:
+# In[4]:
+
+
+# def determine_size():
+#     while True:
+#         try:
+#             value = int(input("""How big should I make the playboard? \n provide me with a number between 10-12 to define the witdh and length of the board.
+#             \n\n\n Type 99 to exit the game \n\n"""))
+#         except ValueError:
+#             print("Sorry, I didn't understand that.")
+#             continue
+
+#         if value == 99:
+#             print('You are a coward captain you are leaving our men behind')
+#             exit() 
+#         elif value < 9:
+#             print("Sorry, your response must fall between 10 and 12")
+#             continue
+#         elif value > 12:
+#             print("Sorry, your response must fall between 10 and 12")
+#         else:
+#             break
+#     return value
+
+
+# In[5]:
 
 
 def make_field(x):
@@ -49,7 +63,7 @@ def make_field(x):
     return board,playerboard
 
 
-# In[4]:
+# In[6]:
 
 
 def place_computer_ships(field,ship):
@@ -67,28 +81,21 @@ def place_computer_ships(field,ship):
             
 
 
-# In[5]:
-
-# In[6]:
+# In[7]:
 
 
 def place_ships(field,ships):
     print("Captain the enemy is upon us :(. To the battlestations!")
     for key,size in ships.items(): 
-        y = int(input('Provide the x coordinate for your {0}  :'.format(names_ships.get(key))))
-        x = int(input('Provide the y coordinate for your {0}  :'.format(names_ships.get(key))))
-#         field[int(x)][int(y)] = key
-
-        
-
-
+        y = click.prompt('Provide the x coordinate for your {0}'.format(names_ships.get(key)), type=click.IntRange(0, 9))
+        x = click.prompt('Provide the y coordinate for your {0}'.format(names_ships.get(key)), type=click.IntRange(0, 9))
         for i in range(0,size):
             field[x-i][y] = key
     
     return field
 
 
-# In[7]:
+# In[8]:
 
 
 def location_check(location,field,size):
@@ -101,11 +108,7 @@ def location_check(location,field,size):
         return location
 
 
-# ###### field = enemy
-# y_loc = 10
-# size = 5
-
-# In[8]:
+# In[2]:
 
 
 def shooting(player,field):
@@ -115,29 +118,55 @@ def shooting(player,field):
         return int(y_shot),int(x_shot) 
     
     else:
-        while True:
-            try:
-                x_shot = int(input('Sir provide the fire X coordinates between 0 and 9 : '))
-                y_shot = int(input('Sir provide the fire Y coordinates between 0 and 9 : '))
-                cond = (y_shot == 99) or (x_shot == 99)
-            except ValueError:
-                print("Sorry captain I don't understand your orders")
-                continue
+        x_shot = click.prompt("""Sir provide the fire X coordinates between 0 and 9: press 10 to quit""", type=click.IntRange(0, 9))
+        y_shot = click.prompt("""Sir provide the fire X coordinates between 0 and 9: press 10 to quit """, type=click.IntRange(0, 9))
+        
+        
+        
+        if x_shot == 13 or y_shot == 13:
+            print('See you later captain chickenshit :)')
+            exit()
+        
+        else:
+            return int(y_shot),int(x_shot) 
+
+
+# In[10]:
+
+
+# def shooting(player,field):
+#     if player != 'Human':
+#         x_shot = rand.randint(0,len(field)-1)
+#         y_shot = rand.randint(0,len(field)-1)
+#         return int(y_shot),int(x_shot) 
+    
+#     else:
+#         while True:
+#             try:
+#                 x_shot = int(input('Sir provide the fire X coordinates between 0 and 9 : '))
+#                 y_shot = int(input('Sir provide the fire Y coordinates between 0 and 9 : '))
+# #                 cond = (y_shot == 99) or (x_shot == 99)
+#             except ValueError:
+#                 print("Sorry captain I don't understand your orders")
+#                 continue
             
-            if(y_shot == 99 or x_shot == 99):
-                print('You are a coward captain you are leaving our men behind')
-                break 
-            if y_shot < 0 or x_shot <0 :
-                print("Your coordinate is not on the map")
-                continue
-            elif  (x_shot > 9) or  (y_shot > 9):
-                print("Your coordinate is not on the map")
-            else:
-                return int(y_shot),int(x_shot) 
+#             if(y_shot == 99 or x_shot == 99):
+#                 print('You are a coward captain you are leaving our men behind')
+#                 exit()
+#             if(y_shot == 99 or x_shot == 99):
+#                 print('You are a coward captain you are leaving our men behind')
+#                 exit()     
+#             if y_shot < 0 or x_shot <0 :
+#                 print("Your coordinate is not on the map")
+#                 continue
+#             elif  (x_shot > 9) or  (y_shot > 9):
+#                 print("Your coordinate is not on the map")
+#             else:
+#                 return int(y_shot),int(x_shot) 
     
 
 
-# In[9]:
+# In[10]:
 
 
 def player_move(hitpoints,player):
@@ -145,19 +174,20 @@ def player_move(hitpoints,player):
     space = player[coordinates[0]][coordinates[1]] 
     if space == '0':
         print('We missed Captain, the enemy has {} hitpoints left'.format(hitpoints))
-        space = 'X'
-        return hitpoints
+        player[coordinates[0]][coordinates[1]]  = 'X'
+        return hitpoints,player
     elif space == 'X':
         print('We already hit this space sir')
-        return hitpoints
+        return hitpoints,player
     else: 
         hitpoints -= 1
         print('')
         print('We have hit their {0}, they have: {1} hitpoint left'.format(names_ships.get(space),hitpoints))
-        return hitpoints
+        player[coordinates[0]][coordinates[1]] = 'X'
+        return hitpoints,player
 
 
-# In[10]:
+# In[11]:
 
 
 def enemy_move(hitpoints,enemy):
@@ -165,59 +195,72 @@ def enemy_move(hitpoints,enemy):
     space = enemy[coordinates[0]][coordinates[1]] 
     if space == '0':
         print('The missed us captain , our fleet has {} hitpoints left'.format(hitpoints))
-        space = 'X'
-        return hitpoints
+        enemy[coordinates[0]][coordinates[1]]  = 'X'
+        return hitpoints,enemy
     elif space == 'X':
         print('They have hit the same spot again ')
-        return hitpoints
+        return hitpoints,enemy
     else: 
         hitpoints -= 1
         print('')
         print('\nThey have hit our {0}, our fleet has: {1} hitpoint left\n'.format(names_ships.get(space),hitpoints))
-        return hitpoints
+        enemy[coordinates[0]][coordinates[1]]  = 'X'
+        return hitpoints,enemy
 
 
-# In[15]:
+# In[12]:
 
 
 def play_game():
     
-    enemy,player = make_field(determine_size())
+    field_size = determine_size()
     
-    enemy = place_computer_ships(enemy,ships)
-    
-    player = place_ships(player,ships)
-    
-    hitpoints_enemy = len([j for i in enemy for j in i if j != '0' and j != 'X'])
-    hitpoints_player = len([j for i in player for j in i if j != '0' and j != 'X'])
+    try: 
+        enemy,player = make_field(field_size)
 
-    while hitpoints_enemy or hitpoints_player:
-        hitpoints_player =  enemy_move(hitpoints_player,player)
-        hitpoints_enemy =  player_move(hitpoints_enemy,enemy)
-        if hitpoints_enemy < 1:
-            print('We won the battle captain the sea is ours')
-            break
-        elif hitpoints_player < 1:
-            print('They finished our last ship Captain we are doomed')
-            break
-        if (hitpoints_enemy != 0 or hitpoints_player != 0):
-            continue
+        enemy = place_computer_ships(enemy,ships)
 
-#     print('We have won the battle captain')
+        player = place_ships(player,ships)
+
+        hitpoints_enemy = len([j for i in enemy for j in i if j != '0' and j != 'X'])
+        hitpoints_player = len([j for i in player for j in i if j != '0' and j != 'X'])
+
+        while hitpoints_enemy or hitpoints_player:
+            try:
+                hitpoints_player,player =  enemy_move(hitpoints_player,player)
+                hitpoints_enemy,enemy =  player_move(hitpoints_enemy,enemy)
+                if hitpoints_enemy < 1:
+                    print('We won the battle captain the sea is ours')
+                    break
+                elif hitpoints_player < 1:
+                    print('They finished our last ship Captain we are doomed')
+                    break
+                if (hitpoints_enemy != 0 or hitpoints_player != 0):
+                    continue
+            except (TypeError,ValueError):
+                print('We follow your lead captain')
+    except:
+        print('Thanks or playing')
 
 
 # # play_game()
 
 # play_game()
 
-# In[16]:
+# In[1]:
 
 
 play_game()
 
 
-# In[ ]:
+# In[35]:
 
 
-
+# def main():
+#     play_game()
+# #     game_on(word)
+#     while input("Play Again? (Y/N) ").upper() == "Y": #gives the user the option to play again
+# #         word = get_word()
+#         play_game()
+# main()
 
